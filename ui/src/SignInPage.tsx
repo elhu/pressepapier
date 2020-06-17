4334f27b1d689b69552ef047818b65de7410330b
Vincent Boisard <boisard.v@gmail.com>
2020-06-17 09:09:35 +0200

    Add signup with google logic

--- ui/src/SignInPage.tsx
+++ ui/src/SignInPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { GoogleLoginButton } from 'react-social-login-buttons';
+
+import firebase from './utils/firebase';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -28,8 +30,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SignInPage: React.SFC = () => {
+const SignInPage: React.FC = () => {
   const classes = useStyles();
+
+  React.useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => console.log(user));
+  }, []);
+
+  const handleGoogleSignup = React.useCallback(async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    try {
+      const currentUser = await firebase.auth().currentUser;
+      const res = currentUser
+        ? await currentUser.linkWithPopup(provider)
+        : await firebase.auth().signInWithPopup(provider);
+      console.log('C');
+
+      console.log('res: ', res);
+    } catch (err) {
+      console.log('err: ', err);
+    }
+  }, []);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -42,7 +64,7 @@ const SignInPage: React.SFC = () => {
         </Typography>
         <Grid container className={classes.social}>
           <Grid item xs={12}>
-            <GoogleLoginButton className={classes.google} />
+            <GoogleLoginButton className={classes.google} onClick={handleGoogleSignup} />
           </Grid>
         </Grid>
       </div>
