89a42c885db4b9bf42313dc46aa9d202397030d3
Vincent Boisard <boisard.v@gmail.com>
2020-06-24 19:56:03 +0200

    Add DB migration for clipboards table

---
+++ backend/db/migrations/20200624191910_add_clipboards_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE clipboards
+(
+  id int NOT NULL,
+  data text NOT NULL,
+  user_uuid VARCHAR(255),
+  PRIMARY KEY(id)
+);
+CREATE INDEX clipboards_user_uuid ON clipboards (user_uuid);
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE clipboards;
